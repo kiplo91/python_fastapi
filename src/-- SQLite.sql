0b3e6794c9f548c2c44ec24b9dbd99443d4547e8
kiplo91 <ahmadfaisal1991@gmail.com>
2022-12-11 17:59:05 +0800

    first commit

---
+++ src/-- SQLite.sql
@@ -0,0 +1,3 @@
+-- SQLite
+INSERT INTO customermodel (id, name, secret_name, age)
+VALUES ('faisal','superman',23);
---
+++ src/main.py
@@ -0,0 +1,69 @@
+from typing import Union
+
+from fastapi  import FastAPI,Depends,Response
+
+from pydantic import BaseModel
+
+from database import  CustomerModel,engine
+from sqlmodel import Session,select
+from models import Customer
+
+app = FastAPI()
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+@app.get('/customers/')
+def customers(session:Session = Depends(get_session)):
+    stmt = select(CustomerModel)
+    result = session.exec(stmt).all()
+    return result
+
+@app.post('/customer/register')
+def register(customer:CustomerModel,session:Session = Depends(get_session)):
+    session.add(customer)
+    session.commit()
+    session.refresh(customer)
+    return customer
+
+@app.get('/customer/{id}')
+def view(id:int,response:Response,session:Session = Depends(get_session)):
+    customer = session.get(CustomerModel,id)
+    if customer is None:
+        response.status_code=404
+        return "Customer Not Found"
+    return customer
+
+@app.put('/customer/{id}')
+def edit( updated_customer: Customer,id:int,response:Response,session:Session=Depends(get_session)):
+    customer = session.get(CustomerModel,id)
+
+    #check if trac exist
+    if customer is None:
+        response.status_code=404
+        return "Track Not Found"
+
+    #update track data
+    customer_dict = updated_customer.dict(exclude_unset=True)
+    for key,val in customer_dict.items():
+        setattr(customer,key,val)
+    
+    session.add(customer)
+    session.commit()
+    session.refresh(customer)
+    return customer
+    
+
+
+@app.delete('/customer/{id}')
+def delete(id:int, response:Response,session:Session=Depends(get_session)):
+    customer = session.get(CustomerModel,id)
+
+    if customer is None:
+        response.status_code=404
+        return "Customer Not Found"
+    
+    session.delete(customer)
+    session.commit()
+    return Response(status_code=200)
