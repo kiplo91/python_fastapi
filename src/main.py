d1ba47f4e24e38ac763a1731795244c0cccfcc05
kiplo91 <ahmadfaisal1991@gmail.com>
2022-12-11 18:46:59 +0800

    fix cors issues

--- src/main.py
+++ src/main.py
@@ -10,6 +10,21 @@ from models import Customer
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 def get_session():
     with Session(engine) as session:
         yield session
